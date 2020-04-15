3f468e3a472202e1413c87cbf02196408bdd2a37
umeshror <sarukumesh@gmail.com>
2020-04-15 19:08:57 +0530

    drf refactored

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ PROJECT_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardi
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'u5x+l6m0(si47=p7#s-+9ql*9um=#ey@@^%md4xjcrb(l815yx')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
@@ -36,6 +36,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-Party Apps
+    'rest_framework',
+
+    # Local Apps
+    'apps.api'
 ]
 
 MIDDLEWARE = [
@@ -80,13 +86,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #     }
 # else:
 DATABASES = {
-'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': os.environ.get('MYSQL_DATABASE', 'auth_db'),
-    'USER': os.environ.get('MYSQL_USER', 'auth_admin'),
-    'PASSWORD': os.environ.get('MYSQL_ROOT_PASSWORD', 'Monday#123'),
-    'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
-    'PORT': os.environ.get('MYSQL_PORT', '3306')}
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE', 'auth_db'),
+        'USER': os.environ.get('MYSQL_USER', 'auth_admin'),
+        'PASSWORD': os.environ.get('MYSQL_ROOT_PASSWORD', 'Monday#123'),
+        'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
+        'PORT': os.environ.get('MYSQL_PORT', '3306')}
 }
 
 # Password validation
