583ed1def3a0032431bc3a7ac1e13c476ac96172
Umesh Saruk <sarukumesh@gmail.com>
2020-06-14 05:57:54 +0530

    static volume added

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
-ENV = os.environ.get('ENV', True)
+ENV = os.environ.get('ENV', 'local')
 
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
